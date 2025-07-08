7160115ceb08a83c27315fb2acb0089215228e17
Aadil Ali <61555486+aadilaliofficial@users.noreply.github.com>
2025-07-08 18:03:50 +0530

    Delete models directory

--- models/budget.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const BudgetSchema = new mongoose.Schema({
-  category: String,
-  amount: Number,
-  month: String, 
-});
-
-export const Budget = mongoose.models.Budget || mongoose.model('Budget', BudgetSchema);
